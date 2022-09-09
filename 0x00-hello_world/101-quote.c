06de138620b42da4a8fa3e58262232cff170932d
mahlet <akotet2123@gmail.com>
2022-09-09 06:12:13 -0700

    task 0x03

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+/*
+* main - prints the given string 
+*
+* Return: 1
+*
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	Dora = char
+	Korpar =char
+
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return 1;
+
+}
