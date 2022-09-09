6cab4721ae734f5c42a4f50c6934ac85b04de0ef
mahlet <akotet2123@gmail.com>
2022-09-08 21:08:48 -0700

    task 0x03

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * return: always 0 (success)
+ */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
