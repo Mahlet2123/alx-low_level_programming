bc994cfb350104a8d49978e68a8e8e7d51619ee2
mahlet <akotet2123@gmail.com>
2022-09-12 12:39:46 -0700

    task_c_0x02

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,26 +10,23 @@ int main(void)
 {
 	int x;
 	int y;
-	int z;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			for (z = 0; z < 10; z++)
+			if (x < y)
 			{
-				if (x < y && y < z)
-				{
-					putchar(x + '0');
-					putchar(y + '0');
-					putchar(z + '0');
-				if (x < 7)
-				{
-					putchar(44);
-					putchar(32);
-				}
-
-				}
+				putchar(x + '0');
+				putchar(x + '0');
+				putchar(32);
+				putchar(x + '0');
+				putchar(y + '0');
+					if (x < 8)
+					{
+						putchar(44);
+						putchar(32);
+					}
 			}
 		}
 	}
