5db58521213a1c2a86bf64dd7aa714b33ff52c9f
mahlet <akotet2123@gmail.com>
2022-09-12 05:04:45 -0700

    task_c_0x02

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++) 
-		{
+	for (x = 0; x <= 9; x++)
+	{
 		putchar(x + '0');
-		if (x < 9) 
-                {
+		if (x < 9)
+		{
 			putchar(44);
 			putchar(32);
 		}
-	}	
+	}
 	putchar(10);
 	return (0);
 }
