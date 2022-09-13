7f4199a6b99e03ae2f3b54e9a07536ad19ee586c
mahlet <akotet2123@gmail.com>
2022-09-12 23:49:30 -0700

    task 0x03

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+
+int main()
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the 3rd exercise for teh C programming language
