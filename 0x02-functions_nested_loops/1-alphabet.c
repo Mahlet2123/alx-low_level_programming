d23c7965c1a0f361250220bfc984c0569b9511de
mahlet <akotet2123@gmail.com>
2022-09-13 11:56:51 -0700

    task 0x03

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+
+	char i = 'a';
+
+	for ( ; i <= 'z' ; i++)
+
+		_putchar (i);
+
+
+
+	_putchar ('\n');
+
+}
