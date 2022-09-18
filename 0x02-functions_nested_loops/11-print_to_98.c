d3a8ff1f1a114edbb7411515df2858fcecd0c731
mahlet <akotet2123@gmail.com>
2022-09-18 12:23:41 -0700

    task 0x03

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,25 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 		}
+	/*printf("98");*/
 	}
 	if (n > 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 		}
+	/*printf("98");*/
 	}
 	if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 	printf("\n");
 }
