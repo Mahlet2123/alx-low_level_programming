c171611fffe7660a568c8842baa0b11933089a13
mahlet <akotet2123@gmail.com>
2022-09-24 02:21:06 -0700

    task 0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,23 @@
 
 /**
  *_strncpy- function that copies a string
- * @dest: destination string
- * @src: source string
+ *@dest: destination string
+ *@src: source string
  *@n: number
  *Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	/*int i;
-
-	for (i = 0; i < n && dest[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	if (n > l)
-		dest[i] = '\0';*/
+/*
+ *	int i;
+ *
+ *	for (i = 0; i < n && dest[i] != '\0'; i++)
+ *	{
+ *		dest[i] = src[i];
+ *	}
+ *	if (n > l)
+ *		dest[i] = '\0';
+ */
 strncpy(dest, src, n);
 return (dest);
 }
