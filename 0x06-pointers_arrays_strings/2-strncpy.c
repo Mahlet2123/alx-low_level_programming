f6afb2d1c1389e2f18cc824a2366487a6322e10a
mahlet <akotet2123@gmail.com>
2022-09-24 01:43:52 -0700

    task 0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- *_strncpy- function that concatenates two strings
+ *_strncpy- function that copies a string
  * @dest: destination string
  * @src: source string
  *@n: number
@@ -12,9 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = '\0';
 return (dest);
 }
