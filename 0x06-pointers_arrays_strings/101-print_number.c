1df67282fd6436e2c927c6074516a70173151ecd
mahlet <akotet2123@gmail.com>
2022-09-24 06:41:06 -0700

    task 0x06

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,11 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number- prints an integer.
+ * @n: an integer
+ *Return: void
  */
 void print_number(int n)
 {
-	unsigned int i;
-
-	i = n;
-	if (n < 0)
-	{
-	_putchar('-');
-	i = -n;
-	}
-	if (i / 10 != 0)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+	_putchar(n + '0');
 }
+
