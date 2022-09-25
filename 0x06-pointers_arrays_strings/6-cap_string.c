f2d3eee6af04bbe798218fde9f94768cf98a9600
mahlet <akotet2123@gmail.com>
2022-09-25 05:37:00 -0700

    task 0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,11 +24,12 @@ char *cap_string(char *str)
 				continue;
 			}
 		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+/*		else
+*		{
+*			if (str[i] >= 'A' && str[i] <= 'Z')
+*				str[i] = str[i] + 32;
+*		}
+*/
 	}
 	return (str);
 }
