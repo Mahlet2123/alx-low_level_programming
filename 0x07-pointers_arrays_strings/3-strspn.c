a41de094581aa06696217d52e9f0d87d5251bca6
mahlet <akotet2123@gmail.com>
2022-09-26 14:07:11 -0700

    task 0x06

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,15 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; i <= strlen(accept); i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; j <= strlen(s); j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (s[j] == accept[i])
-			{
+			if (s[i] == accept[j])
 				break;
-			}
 		}
+		if (!accept[j])
+			break;
 	}
-return (i);
+	return (i);
 }
