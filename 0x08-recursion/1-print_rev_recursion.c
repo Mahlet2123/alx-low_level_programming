72090091d9001c3597d555d2e084fd5b4ad007d4
mahlet <akotet2123@gmail.com>
2022-09-28 11:51:08 -0700

    task 0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int n = 0;	
-	
-	if (s[n] == '\0')
+	if (*s != '\0')
 	{
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_print_rev_recursion(s + 1);
 }
