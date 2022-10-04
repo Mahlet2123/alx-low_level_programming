f9754913e3df688370a88793ede67535960d26dc
mahlet <akotet2123@gmail.com>
2022-10-03 22:47:05 -0700

    task 0x0B

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 /**
  * create_array- creates an array of chars
- *
+ * @size: the size of the memory to print
+ * @c: the character that fills the array
  * Return: NULL (if size = 0)
  */
 char *create_array(unsigned int size, char c)
@@ -12,10 +13,6 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(size);
 	if (str == 0)
-	{
-		return (NULL);
-	}
-	if (str == NULL)
 	{
 		return (NULL);
 	}
