7aefab083a4334747e54b28d21e551dfac151fb6
mahlet <akotet2123@gmail.com>
2022-10-03 22:28:37 -0700

    task 0x0B

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array- creates an array of chars
+ *
+ * Return: NULL (if size = 0)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(size);
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	if (str == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
