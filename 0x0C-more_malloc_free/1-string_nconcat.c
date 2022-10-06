3151a26bd04b06917531608037e4c50c16a26993
mahlet <akotet2123@gmail.com>
2022-10-06 12:39:01 -0700

    task 0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= l2)
 		n = l2;
 	i = (l1 + n);
-	s = malloc(sizeof(char) * i + 1);
+	s = malloc(i + 1);
 	if (s == NULL)
 	{
 		return (NULL);
