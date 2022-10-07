e8a11e84ec21ef397adfc08ddf83d68971b69dfc
mahlet <akotet2123@gmail.com>
2022-10-07 10:29:12 -0700

    task 0x0C

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,12 +8,12 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	char *p;
 
 	if (new_size > old_size)
 	{
 		p = malloc(new_size);
-		free(p);
+		free(ptr);
 		return (p);
 	}
 	if (new_size == old_size)
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
-		free(p);
+		free(ptr);
 		return (p);
 	}
 	if (new_size == 0 && ptr != NULL)
