b6f4d05d359932aadd3700e32bcb58d6eaf464bd
mahlet <akotet2123@gmail.com>
2022-10-10 10:31:37 -0700

    task 0x03

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog- Poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: structure for dog info
+ */
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+};
