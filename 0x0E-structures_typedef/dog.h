971f6d8682359c6153d3ff45a68f7d9288f35c34
mahlet <akotet2123@gmail.com>
2022-10-10 12:44:14 -0700

    task 0x03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: structure for dog info
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
