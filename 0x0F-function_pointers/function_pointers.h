64b9722b9d340a7cc658df10bac48c290e110855
mahlet <akotet2123@gmail.com>
2022-10-12 12:47:10 -0700

    task 0x08

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stddef.h> 
 int main(void);
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void print(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
