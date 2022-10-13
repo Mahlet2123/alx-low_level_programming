b4d919f202fa062c62c802c4c5438701df3514f3
mahlet <akotet2123@gmail.com>
2022-10-13 07:23:09 -0700

    task 0x08

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, unsigned int));
-		printf("%c", *separator);
+		if (i < (n - 1))
+			printf("%c", *separator);
 	}
 	printf("\n");
 	va_end(ap);
