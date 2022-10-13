cf6e6bf56e006f9074d1441b81c6b251ab223711
mahlet <akotet2123@gmail.com>
2022-10-13 10:13:12 -0700

    task 0x08

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(ap, int));
 		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%c", *separator);
+			printf("%s", separator);
 		}
 	}
 	putchar('\n');
