d6d3d0b7b0c5b33d0093fc24deb8864a7149edac
mahlet <akotet2123@gmail.com>
2022-10-13 10:56:15 -0700

    task 0x08

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(string, char*);
 		printf("%s", str);
-		if (i < (n - 1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
