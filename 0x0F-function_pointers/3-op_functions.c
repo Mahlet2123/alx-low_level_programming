6e9fde43ba6a6dfd31ccecbba328eef2ee4edc99
mahlet <akotet2123@gmail.com>
2022-10-14 15:48:29 -0700

    task 0x08

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,3 +11,51 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
+/**
+ * op_sub- calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: diffrence of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- calculates the multiplication of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- calculates the division two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- calculates the modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
