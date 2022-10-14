de2a06e9a1af0fd79dcb6fc1123b4368b9a93384
mahlet <akotet2123@gmail.com>
2022-10-14 06:00:00 -0700

    task 0x08

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,31 +11,36 @@
 void print_all(const char * const format, ...)
 {
 	va_list p;
-	unsigned int i = 0;
+	unsigned int i = 0, j, c = 0;
 	char *str;
+	const char f_arg[] = "cifs";
 
 	va_start(p, format);
 	while (format && format[i])
-
 	{
+		j = 0;
+		while (f_arg[j])
+		{
+			if (format[i] == f_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(p, int));
-			printf(", ");
+			printf("%c", va_arg(p, int)), c = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(p, int));
-			printf(", ");
+			printf("%d", va_arg(p, int)), c = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(p, double));
-			printf(", ");
-
+			printf("%f", va_arg(p, double)), c = 1;
 			break;
 		case 's':
-			str = va_arg(p, char *);
-			if (!str)
+			str = va_arg(p, char *), c = 1;
+			if (str == NULL)
 			{
 				printf("(nil)");
 				break;
