1537ab23732469e1841e90e0b0f94f3bc646266d
mahlet <akotet2123@gmail.com>
2022-10-21 11:17:48 -0700

    task 0x12

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /**
  * list_len- returns the number of elements in a linked list_t list.
- *
+ * @h: singly linked list
  * Return: Number of elements
  */
 
@@ -12,7 +12,7 @@ size_t list_len(const list_t *h)
 
 	for (i = 0; h; i++)
 	{
-		h = h ->next;
+		h = h->next;
 	}
 	return (i);
 }
