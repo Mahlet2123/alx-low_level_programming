a986f69c5c0cf08bd3bf4120b6e6d79268cc40e5
mahlet <akotet2123@gmail.com>
2022-10-25 03:15:23 -0700

    task 0x13

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
         ptr = *head;
+	if ((ptr->next) == NULL)
+		return (NULL);
         if (ptr->next != NULL)
         {
             while (idx)
@@ -27,10 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
                 ptr = ptr->next;
                 idx--;
             }
-	}
-	else
-		return (NULL);
         new->next = ptr->next;
         ptr->next = new;
+	}
 	return (new);
 }
