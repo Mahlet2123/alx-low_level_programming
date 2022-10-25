25b62fff4d56acf89c4cbebac7772301c64d852a
mahlet <akotet2123@gmail.com>
2022-10-24 21:09:29 -0700

    task 0x13

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,10 +11,15 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	temp = *head;
-	n = (*head)->n;
-	temp = temp->next;
-	free(*head);
-	*head = temp;
+	if (*head == NULL)
+		return (0);
+	else
+	{
+		temp = *head;
+		n = (*head)->n;
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
 	return (n);
 }
