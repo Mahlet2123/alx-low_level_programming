51f958683b1110b4bc10aecc85b071c9781ac471
mahlet <akotet2123@gmail.com>
2022-10-24 21:53:10 -0700

    task 0x13

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,18 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	temp = head;
-	while (index)
+	if (temp->next != NULL)
 	{
-		temp = temp->next;
-		index--;
+		while (index)
+		{
+			temp = temp->next;
+			index--;
+		}
 	}
+	else
+	{
+		return (NULL);
+	}
+
 	return (temp);
 }
