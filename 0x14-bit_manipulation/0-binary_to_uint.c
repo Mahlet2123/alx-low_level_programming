531cb69dad857084a0aff79334306405d85ece3e
mahlet <akotet2123@gmail.com>
2022-10-27 07:32:21 -0700

    task 0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,25 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len, i;
-	unsigned int val = 0, n = 1;
+	unsigned int val = 0;
+	int i = 0;
 
-	len = strlen(b);
 	if (b == NULL)
 		return (0);
-	for (i = (len - 1); i >= 0; i--)
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (b[i] == '1')
-		{	
-			val = val + n;
-			n = n * 2;
-		}
-		else if (b[i] == '0')
-		{
-			i++;
-		}
-		else 
-			return (0);
+		val <<= 1;
+		val += b[i]-'0';
+		i++;
 	}
+	
 	return (val);
 }
