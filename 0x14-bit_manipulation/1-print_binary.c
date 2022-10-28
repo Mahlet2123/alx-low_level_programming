65331085a07720a25e617ab0483b98c850ba193a
mahlet <akotet2123@gmail.com>
2022-10-27 17:57:18 -0700

    task 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary- prints the binary representation of a number.
- *
+ * @n: unsigned long int n in base 10
  * Return: void
  */
 
@@ -10,5 +10,5 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	_putchar((n & 1) + '0');	
+	_putchar((n & 1) + '0');
 }
