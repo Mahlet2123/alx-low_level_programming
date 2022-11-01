b0f54338f09f1e5853a91f27aee785c12347921d
mahlet <akotet2123@gmail.com>
2022-11-01 09:53:35 -0700

    task 0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,15 +14,24 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
+		exit(100);
+	}
 	while (rd)
 	{
 		rd = read(fd_1, buff, 1024);
 		if (rd == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		wr = write(fd_2, buff, rd);
 		if (wr == -1 || wr != rd)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
