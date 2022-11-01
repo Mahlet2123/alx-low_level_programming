b7acfd36cc522e4739e90771ead4ccb6cdf1ff47
mahlet <akotet2123@gmail.com>
2022-11-01 09:48:20 -0700

    task 0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,27 +11,27 @@ int main(int argc, char *argv[])
 	char buff[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_1 = open(argv[1], O_RDONLY);
 	if (fd_1 == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd_2 == -1)
-		dprintf(2, "Error: Can't close fd %s\n", argv[2]), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]), exit(100);
 	while (rd)
 	{
 		rd = read(fd_1, buff, 1024);
 		if (rd == -1)
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		wr = write(fd_2, buff, rd);
 		if (wr == -1 || wr != rd)
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	close(fd_1);
 	if (fd_1 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_1), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1), exit(100);
 	close(fd_2);
 	if (fd_2 == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_2), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2), exit(100);
 	return (0);
 }
