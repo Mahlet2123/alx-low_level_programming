7fbec02896ac30d917b58c0b489170c214504e5a
mahlet <akotet2123@gmail.com>
2022-11-01 11:41:48 -0700

    task 0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	fd_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
