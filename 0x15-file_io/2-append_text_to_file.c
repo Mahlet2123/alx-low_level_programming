822d0d413477f7c4389a74041e88be736bec272f
mahlet <akotet2123@gmail.com>
2022-11-01 04:23:58 -0700

    task 0x15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wr, len;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
-	wr = write(fd, text_content, len)
+	wr = write(fd, text_content, len);
 	if (wr == -1)
 		return (-1);
 	close(fd);
