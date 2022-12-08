0da3f004b0b036b3400bd739a81cb1630e0e2e19
mahlet <akotet2123@gmail.com>
2022-12-08 10:26:35 -0800

    task 0x17

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
  * dlistint_len- returns the number of elements in a linked dlistint_t list
+ * @h: pointer to the linked list
  * Return: the number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
