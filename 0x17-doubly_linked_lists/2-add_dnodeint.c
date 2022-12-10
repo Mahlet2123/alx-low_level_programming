f60ed8794353adc5b15d18a6b509808031d3768d
mahlet <akotet2123@gmail.com>
2022-12-09 16:34:52 -0800

    task 0x17

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = (*head);
 	new_node->prev = NULL;
 	*head = new_node;
-	if (new_node->next != NULL)
-		(new_node->next)->prev = new_node;
+/**
+ * if (new_node->next != NULL)
+*		(new_node->next)->prev = new_node;
+*/
 	return (*head);
 }
