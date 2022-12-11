3e022112cc3c82d923f363b1b60c1b57d34c0267
mahlet <akotet2123@gmail.com>
2022-12-10 18:41:26 -0800

    task_c_0x02

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,21 +14,22 @@ int main(void)
 		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 9; k++)
-			{	
-		 		for (l = 0; l <= 9; l++)
+			{
+				for (l = 0; l <= 9; l++)
 				{
-					if (i = k && j = l)
+					if (i == k && j == l)
 						continue;
-						putchar(i + '0');
-						putchar(j + '0');
-						putchar(' ');
-						putchar(k + '0');
-						putchar(l + '0');
-						putchar(',');
-						putchar(' ');
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-return (0);
+	putchar("\n");
+	return (0);
 }
