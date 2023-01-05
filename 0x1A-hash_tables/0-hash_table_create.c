01a2df275066a4cdf265e3706c1b8a5da1848151
mahlet <akotet2123@gmail.com>
2023-01-05 05:59:19 -0800

    task 0x1A

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- creates a hash table.
+ *
+ * Return: a pointer to the newly created hash table
+ * If something went wrong, returns NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t * new_hash_table;
+	
+	if (!size)
+		return NULL;
+	
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+		return NULL;
+	
+	new_hash_table->size = size;
+	new_hash_table->array = malloc(sizeof(hash_node_t*) * size);
+	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
+		return NULL;
+	}
+
+	return new_hash_table;
+}
