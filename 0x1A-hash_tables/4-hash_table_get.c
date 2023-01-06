78cf4b85e62871d1d78b49521c819b84af69d8b3
mahlet <akotet2123@gmail.com>
2023-01-06 14:51:38 -0800

    task 0x1A

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,16 +9,17 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
-	hash_node_t *entry;
+	hash_node_t *item;
 
 	if (!ht || !key || !(*key))
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	entry = ht->array[index];
-	if (entry)
+	item = ht->array[index];
+	if (item)
 	{
-		if (strcmp(entry->key, key) == 0)
-			return (entry->value);
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+		item = item->next;
 	}
 	return (NULL);
 }
