b5445beaec0e06d98fee7a8d20d1e58887506a3a
mahlet <akotet2123@gmail.com>
2023-01-06 07:19:25 -0800

    task 0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	new_item = create_new_item(key, value);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	/** try to look up the key index*/
 	entry = ht->array[index];
 	if (entry == NULL)
