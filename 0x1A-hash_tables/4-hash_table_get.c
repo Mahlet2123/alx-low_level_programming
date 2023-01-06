096180b167d6fbdc579645995ef9dd39e1503ed6
mahlet <akotet2123@gmail.com>
2023-01-06 14:45:07 -0800

    task 0x1A

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	entry = ht->array[index];
-	if (entry == NULL)
-		return (NULL);
-	return (entry->value);
+	if (entry)
+	{
+		if (strcmp(entry->key, key) == 0)
+			return (entry->value);
+	}
+	return (NULL);
 }
