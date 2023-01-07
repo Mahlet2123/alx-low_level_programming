b8948c93c885fa3cf76d467feafe3aedb9ab28da
mahlet <akotet2123@gmail.com>
2023-01-06 17:30:19 -0800

    task 0x1A

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete- deletes a hash table
+ * @ht: ht is the hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *item, *temp;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+		if (item)
+		{
+			temp = item;
+			free(temp->key);
+			free(temp->value);
+			free(item);
+			free(temp);
+			item = item->next;
+		}
+	}
+	free(ht->array);	
+	free(ht);
+}
