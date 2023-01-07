9937ebc49f34500b28746e9bfe1e56ca9fe5c406
mahlet <akotet2123@gmail.com>
2023-01-06 16:09:52 -0800

    task 0x1A

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print- prints a hash table
+ * @ht: the hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	hash_node_t *item;
+
+	if(!ht)
+	{
+		printf("{}\n");
+		exit(0);
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if ((item = ht->array[i]))
+			printf("'%s': '%s'", item->key, item->value);
+	}
+	printf("}\n");
+}
