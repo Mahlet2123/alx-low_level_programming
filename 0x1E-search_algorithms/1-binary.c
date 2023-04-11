ea5ed9ed5cbf3048b6fdb00110dcee1f448ca967
mahlet <akotet2123@gmail.com>
2023-04-11 02:25:07 -0700

    betty fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -22,7 +22,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (l <= r)
 	{
-		m =(l + r) / 2;
+		m = (l + r) / 2;
 		printf("Searching in array:");
 		for (i = l; i < r; i++)
 			printf(" %d,", array[i]);
