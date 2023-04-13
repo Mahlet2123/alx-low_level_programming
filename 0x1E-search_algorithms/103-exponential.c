8625d9a3d15133084b79fdedffc6a24f171014cd
Mahlet Seifu <110999391+Mahlet2123@users.noreply.github.com>
2023-04-13 13:29:41 +0300

    added binary search function

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -29,41 +29,42 @@ int exponential_search(int *array, size_t size, int value)
 	return (ret == -1 ? ret : ret + (int)i);
 }
 
-#include "search_algos.h"
-
 /**
- * binary_search - searches for a value in a sorted array of integers using BSA
- * @array: pointer to first element of array to search
- * @size: size of the array
- * @value: value to seach for
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
  *
- * Return: index of value location or -1
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located
+ *
+ * If value is not present in array or if array is NULL,
+ * your function must return -1
+ *
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t start = 0, end = size - 1, mid;
-	size_t i;
+	size_t l = 0, r = size - 1, i;
+	int m;
 
-	if (!array || !size)
+	if (array == NULL)
 		return (-1);
-
-	while (start <= end)
+	while (l <= r)
 	{
-		mid = (start + end) / 2;
+		m = (l + r) / 2;
 		printf("Searching in array:");
-		for (i = start; i <= end; i++)
-		{
-			if (i < end)
-				printf(" %d,", array[i]);
-			else
-				printf(" %d\n", array[i]);
-		}
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] > value)
-			end = mid - 1;
+		for (i = l; i < r; i++)
+			printf(" %d,", array[i]);
+		if (i == r)
+			printf(" %d", array[i]);
+		printf("\n");
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
 		else
-			start = mid + 1;
+			return (m);
 	}
 	return (-1);
 }
