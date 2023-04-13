4ce1642433077ad07f2113994e32fb490746fa31
mahlet <akotet2123@gmail.com>
2023-04-13 12:23:45 -0700

    PEP fix

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -19,42 +19,40 @@
 
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t i, mid;
-    int res;
+	size_t i, mid;
+	int res;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
 
-    if (size == 1 && array[0] != value)
-        return (-1);
+	if (size == 1 && array[0] != value)
+		return (-1);
 
-    mid = (size - 1) / 2;
-    if (array[mid] == value)
-    {
-        if (array[mid - 1] == value)
-            return (advanced_binary(array, mid + 1, value));
-        else
-            return (mid);
-    }
-    else if (array[mid] < value)
-    {
-        res = advanced_binary(array + mid + 1, size - mid - 1, value);
-        if (res == -1)
-            return (-1);
-        else
-            return (res + mid + 1);
-    }
-    else
-    {
-        return (advanced_binary(array, mid + 1, value));
-    }
+	mid = (size - 1) / 2;
+	if (array[mid] == value)
+	{
+		if (array[mid - 1] == value)
+			return (advanced_binary(array, mid + 1, value));
+		else
+			return (mid);
+	}
+	else if (array[mid] < value)
+	{
+		res = advanced_binary(array + mid + 1, size - mid - 1, value);
+		if (res == -1)
+			return (-1);
+		else
+			return (res + mid + 1);
+	}
+	else
+		return (advanced_binary(array, mid + 1, value));
 }
